0d9a4add01c7ef5605d82b15d0ec4cd2e0fbb24b
Hvhgeheheh <100161903+Hvhgeheheh@users.noreply.github.com>
2025-02-16 22:41:03 +0500

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ from dotenv import load_dotenv
 load_dotenv()  # Загружаем .env
 
 token = os.getenv("TOKEN")  # Получаем токен
-allowed_user_id = set(int(uid) for uid in os.getenv("ALLOWED_USERS", "").split(",") if uid.strip().isdigit())
+allowed_user_id = set(map(int, filter(None, os.getenv("ALLOWED_USERS", "").split(","))))
